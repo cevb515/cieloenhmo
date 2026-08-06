fe9ce7eac8a1b1c054fde5aaea30a2ff1c24a3a6
cevb515 <cevb515@gmail.com>
2026-08-05 17:48:39 -0700

    Agrega datos de clima a la pagina

---
+++ scripts/fetch_weather.py
@@ -0,0 +1,31 @@
+import json
+import urllib.request
+
+LATITUDE = 29.0729
+LONGITUDE = -110.9559
+
+url = (
+    "https://api.open-meteo.com/v1/forecast"
+    f"?latitude={LATITUDE}&longitude={LONGITUDE}"
+    "&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m"
+    "&timezone=America/Hermosillo"
+)
+
+with urllib.request.urlopen(url) as response:
+    data = json.load(response)
+
+current = data["current"]
+
+output = {
+    "temperatura": current["temperature_2m"],
+    "sensacion": current["apparent_temperature"],
+    "humedad": current["relative_humidity_2m"],
+    "viento": current["wind_speed_10m"],
+    "actualizado": current["time"],
+}
+
+with open("data.json", "w", encoding="utf-8") as f:
+    json.dump(output, f, ensure_ascii=False, indent=2)
+
+print("Listo, data.json creado con estos datos:")
+print(output)
